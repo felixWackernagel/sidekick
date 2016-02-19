876aa0ee1e8bbbb4db1f1a468da761e9212560c2
Felix Wackernagel <felix.wackernagel@exedio.com>
2016-02-19 14:46:30 +0100

    some changes

--- sidekick/src/main/java/de/wackernagel/android/sidekick/utils/DeviceUtils.java
+++ sidekick/src/main/java/de/wackernagel/android/sidekick/utils/DeviceUtils.java
@@ -1,10 +1,12 @@
 package de.wackernagel.android.sidekick.utils;
 
 import android.app.Activity;
-import android.content.res.Resources;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
+
+import de.wackernagel.android.sidekick.R;
 
 public class DeviceUtils {
 
@@ -20,11 +22,19 @@ public class DeviceUtils {
         return displaymetrics.heightPixels;
     }
 
-    public static int dpToPx( float dp, @NonNull final Resources res ) {
-        return Math.round( TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, res.getDisplayMetrics() ) );
+    public static int dpToPx( float dp, @NonNull final Context context ) {
+        return Math.round( TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics()) );
     }
 
-    public static float pxToDp( int px, @NonNull final Resources res ) {
-        return (float) px / res.getDisplayMetrics().density;
+    public static float pxToDp( int px, @NonNull final Context context ) {
+        return (float) px / context.getResources().getDisplayMetrics().density;
+    }
+
+    public static boolean isTablet( @NonNull final Context context ) {
+        return context.getResources().getBoolean( R.bool.sidekick_tablet );
+    }
+
+    public static boolean isLandscape( @NonNull final Context context ) {
+        return context.getResources().getBoolean( R.bool.sidekick_landscape );
     }
 }
--- sidekick/src/main/java/de/wackernagel/android/sidekick/utils/NetworkUtils.java
+++ sidekick/src/main/java/de/wackernagel/android/sidekick/utils/NetworkUtils.java
@@ -1,7 +1,28 @@
 package de.wackernagel.android.sidekick.utils;
 
-/**
- * Created by wackernagel on 19.02.16.
- */
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+
 public class NetworkUtils {
+
+    public static boolean isOnline( @NonNull final Context context ) {
+        final ConnectivityManager connectionManager =  (ConnectivityManager) context.getSystemService( Context.CONNECTIVITY_SERVICE );
+        final NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+        return ( networkInfo != null && networkInfo.isConnectedOrConnecting() );
+    }
+
+    public static boolean isOnlineWifi( @NonNull final Context context ) {
+        final ConnectivityManager connectionManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE );
+        final NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnectedOrConnecting() && networkInfo.getType() == ConnectivityManager.TYPE_WIFI );
+    }
+
+    public static boolean isOnlineMobile( @NonNull final Context context ) {
+        final ConnectivityManager connectionManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE );
+        final NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnectedOrConnecting() && networkInfo.getType() == ConnectivityManager.TYPE_MOBILE );
+    }
+
 }
