bd2c60354c011bd46547d11b304e935f453f2778
Felix Wackernagel <felix.wackernagel@gmail.com>
2016-05-08 20:00:41 +0200

    Simplify ContractContentProvider

--- sidekick/src/main/java/de/wackernagel/android/sidekick/frameworks/contentproviderprocessor/contract/ContractContentProvider.java
+++ sidekick/src/main/java/de/wackernagel/android/sidekick/frameworks/contentproviderprocessor/contract/ContractContentProvider.java
@@ -1,6 +1,9 @@
 package de.wackernagel.android.sidekick.frameworks.contentproviderprocessor.contract;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,39 +13,86 @@ import de.wackernagel.android.sidekick.frameworks.contentproviderprocessor.Conte
 
 public abstract class ContractContentProvider extends AbstractContentProvider {
 
-    private final String authority;
-    private final List<TableContract> tables;
-    private final List<JoinContract> joins;
+    // PROCESSOR
+    private final @NonNull String authority;
+    private final List<Contract> contracts;
 
-    public ContractContentProvider( @NonNull final String authority ) {
+    // DATABASE
+    private final List<SQLiteTable> sqLiteTables;
+    private final @NonNull String databaseName;
+    private final int databaseVersion;
+    private final @Nullable SQLiteDatabase.CursorFactory cursorFactory;
+
+    public ContractContentProvider(
+            @NonNull final String authority,
+            @NonNull final String databaseName,
+            int databaseVersion ) {
+        this( authority, databaseName, databaseVersion, null );
+    }
+
+    public ContractContentProvider(
+            @NonNull final String authority,
+            @NonNull final String databaseName,
+            int databaseVersion,
+            @Nullable final SQLiteDatabase.CursorFactory cursorFactory ) {
         this.authority = authority;
-        this.tables = new ArrayList<>();
-        this.joins = new ArrayList<>();
+        this.databaseName = databaseName;
+        this.databaseVersion = databaseVersion;
+        this.cursorFactory = cursorFactory;
+        this.contracts = new ArrayList<>();
+        this.sqLiteTables = new ArrayList<>();
     }
 
-    public boolean addContract( final TableContract tableContract ) {
-        return tables.add( tableContract );
+    public List<SQLiteTable> getSqLiteTables() {
+        return sqLiteTables;
     }
 
-    public boolean addContract( final JoinContract joinContract) {
-        return joins.add( joinContract );
+    /**
+     * The {@link SQLiteTable} is used for the {@link SQLiteTableOpenHelper}.
+     *
+     * @param sqLiteTable table of the database
+     */
+    public void addSQLiteTable( final SQLiteTable sqLiteTable ) {
+        sqLiteTables.add( sqLiteTable );
+    }
+
+    public List<Contract> getContracts() {
+        return contracts;
+    }
+
+    /**
+     * If the {@link Contract} implements the {@link SQLiteTable} interface then
+     * it calls internal {@link #addSQLiteTable(SQLiteTable)}.
+     *
+     * @param contract object to define a processor
+     */
+    public void addContract( final Contract contract ) {
+        if( contract instanceof SQLiteTable ) {
+            addSQLiteTable((SQLiteTable) contract);
+        }
+        contracts.add( contract );
     }
 
     @NonNull
     @Override
     public ContentProviderProcessor[] onCreateContentProviderProcessors() {
-        final ContentProviderProcessor[] processors = new ContentProviderProcessor[ tables.size() + joins.size() ];
-
-        int index = 0;
-        for( int position = 0; position < tables.size(); position++ ) {
-            processors[index] = onResolveProcessor( tables.get( position ) );
-            index++;
-        }
-        for( int position = 0; position < joins.size(); position++ ) {
-            processors[index] = onResolveProcessor( joins.get( position ) );
-            index++;
+        final ContentProviderProcessor[] processors = new ContentProviderProcessor[ contracts.size() ];
+        final int count = contracts.size();
+        for( int position = 0; position < count; position++ ) {
+            processors[position] = onResolveProcessor(contracts.get(position));
         }
         return processors;
+    }
+
+    @NonNull
+    @Override
+    public SQLiteOpenHelper onCreateSQLiteOpenHelper() {
+        return new SQLiteTableOpenHelper(
+                getContext(),
+                databaseName,
+                cursorFactory,
+                databaseVersion,
+                sqLiteTables);
     }
 
     @NonNull
