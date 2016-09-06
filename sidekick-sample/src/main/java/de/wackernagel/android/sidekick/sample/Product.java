528f4f95fff9f64001c37be950c863e8f2744793
Felix Wackernagel <felix.wackernagel@gmail.com>
2016-09-06 16:12:46 +0200

    introduce annotations and annotation processors and sample

---
+++ sidekick-sample/src/main/java/de/wackernagel/android/sidekick/sample/Product.java
@@ -0,0 +1,10 @@
+package de.wackernagel.android.sidekick.sample;
+
+import de.wackernagel.android.sidekick.annotations.Contract;
+
+@Contract
+public abstract class Product {
+
+    abstract String getName();
+
+}
