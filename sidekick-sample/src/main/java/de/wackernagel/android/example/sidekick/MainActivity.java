f14f2ed1d8a446e57f916cdc886f5073b40f55f2
Felix Wackernagel <felix.wackernagel@gmail.com>
2016-11-04 15:38:01 +0100

    example changes

--- sidekick-sample/src/main/java/de/wackernagel/android/example/sidekick/MainActivity.java
+++ sidekick-sample/src/main/java/de/wackernagel/android/example/sidekick/MainActivity.java
@@ -2,55 +2,114 @@ package de.wackernagel.android.example.sidekick;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import de.wackernagel.android.sidekick.compats.ObjectsCompat;
+import de.wackernagel.android.sidekick.frameworks.objectcursor.ObjectLoader;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<MainActivity.SimpleItem>> {
+
+    private final SimpleAdapter adapter = new SimpleAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final SimpleAdapter adapter = new SimpleAdapter();
-        adapter.addItem( "Widgets - TypefaceTextView", TypefaceTextViewActivity.class );
-        adapter.addItem( "Widgets - AspectRatioImageView", AspectRatioImageViewActivity.class );
-        adapter.addItem( "Widgets - CircularRevealView", RevealViewActivity.class );
-        adapter.addItem( "Widgets - IndicatorView", IndicatorActivity.class );
-        adapter.addItem( "Utils - Tooltip", TooltipActivity.class );
-        adapter.addItem( "Utils - ColorFilterUtils", ColorFilterUtilsActivity.class );
-        adapter.addItem( "Utils - Device and Network", DeviceActivity.class );
-        adapter.addItem( "Utils - Drawable Tinting", TintingActivity.class );
-        adapter.addItem( "Helper - Photos", PhotoActivity.class );
-        adapter.addItem( "Resources - Colors", ColorsActivity.class );
-        adapter.addItem( "Frameworks - ContentProviderProcessor", SimpleProviderActivity.class );
+        final RecyclerView recyclerView = ObjectsCompat.requireNonNull(
+                ( RecyclerView ) findViewById(R.id.recyclerView),
+                "No RecyclerView in layout found!" );
+        recyclerView.setLayoutManager( new LinearLayoutManager(this) );
+        recyclerView.setAdapter( adapter );
 
-        final RecyclerView recyclerView = ( RecyclerView ) findViewById(R.id.recyclerView);
-        if( recyclerView != null ) {
-            recyclerView.setLayoutManager( new LinearLayoutManager(this) );
-            recyclerView.setAdapter( adapter );
+        getSupportLoaderManager().initLoader( 0, null, this );
+    }
+
+    @Override
+    public Loader<List<SimpleItem>> onCreateLoader(int id, Bundle args) {
+        if( id != 0 ) {
+            return null;
+        }
+        return new ObjectLoader<List<SimpleItem>>( this ) {
+            @Override
+            public List<SimpleItem> loadInBackground() {
+                SystemClock.sleep( 1000L );
+
+                final List<SimpleItem> list = new ArrayList<>();
+                list.add( new SimpleItem( "Widgets - TypefaceTextView", TypefaceTextViewActivity.class ) );
+                list.add( new SimpleItem( "Widgets - AspectRatioImageView", AspectRatioImageViewActivity.class ) );
+                list.add( new SimpleItem( "Widgets - CircularRevealView", RevealViewActivity.class ) );
+                list.add( new SimpleItem( "Widgets - IndicatorView", IndicatorActivity.class ) );
+                list.add( new SimpleItem( "Utils - Tooltip", TooltipActivity.class ) );
+                list.add( new SimpleItem( "Utils - ColorFilterUtils", ColorFilterUtilsActivity.class ) );
+                list.add( new SimpleItem( "Utils - Device and Network", DeviceActivity.class ) );
+                list.add( new SimpleItem( "Utils - Drawable Tinting", TintingActivity.class ) );
+                list.add( new SimpleItem( "Helper - Photos", PhotoActivity.class ) );
+                list.add( new SimpleItem( "Resources - Colors", ColorsActivity.class ) );
+                list.add( new SimpleItem( "Frameworks - ContentProviderProcessor", SimpleProviderActivity.class ) );
+                return list;
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<SimpleItem>> loader, List<SimpleItem> data) {
+        if( loader.getId() != 0 ) {
+            return;
+        }
+        adapter.addItems( data );
+
+        final ProgressBar progress = ( ProgressBar ) findViewById( R.id.progressBar );
+        progress.setVisibility( View.GONE );
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<SimpleItem>> loader) {
+        if( loader.getId() != 0 ) {
+            return;
+        }
+        adapter.clearItems();
+    }
+
+    public static class SimpleItem {
+        final String name;
+        final Class<?> activity;
+
+        SimpleItem(String name, Class<?> activity) {
+            this.name = name;
+            this.activity = activity;
         }
     }
 
     public static class SimpleAdapter extends RecyclerView.Adapter<SimpleViewHolder> {
-        private ArrayList<String> names = new ArrayList<>();
-        private ArrayList<Class<?>> activities = new ArrayList<>();
+        private ArrayList<SimpleItem> items = new ArrayList<>();
 
-        void addItem( String name, Class<?> activity ) {
-            names.add( name );
-            activities.add( activity );
-            notifyDataSetChanged();
+        void addItems( List<SimpleItem> items ) {
+            this.items.addAll( items );
+            notifyItemRangeInserted( 0, items.size() );
         }
 
-        Class<?> getItem( int position ) {
-            return activities.get( position );
+        void clearItems() {
+            int size = items.size();
+            this.items.clear();
+            notifyItemRangeRemoved(0, size);
+        }
+
+        SimpleItem getItem( int position ) {
+            return items.get( position );
         }
 
         @Override
@@ -61,18 +120,18 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(SimpleViewHolder holder, int position) {
             final int adaptPos = holder.getAdapterPosition();
-            holder.text.setText( names.get( position ) );
+            holder.text.setText( items.get( position ).name );
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    v.getContext().startActivity(new Intent(v.getContext(), getItem(adaptPos)));
+                    v.getContext().startActivity(new Intent(v.getContext(), getItem(adaptPos).activity));
                 }
             });
         }
 
         @Override
         public int getItemCount() {
-            return names.size();
+            return items.size();
         }
     }
 
